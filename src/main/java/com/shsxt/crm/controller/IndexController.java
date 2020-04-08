2c81dfd5821c6c49b04383d7360ef88ec85c01c2
admin <1993610971@qq.com>
2020-04-08 14:51:05 +0800

    初始化文件

---
+++ src/main/java/com/shsxt/crm/controller/IndexController.java
@@ -0,0 +1,26 @@
+package com.shsxt.crm.controller;
+
+import com.shsxt.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController extends BaseController {
+
+    /**
+     * 登录页
+     */
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+
+    /**
+     * 后台管理主页面
+     */
+    @RequestMapping("main")
+    public String main(){
+        return "main";
+    }
+
+}
